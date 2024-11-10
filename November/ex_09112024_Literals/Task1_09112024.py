33bf9019415b40bc78c594b8c0b5d98371c8e119
amitsinha2811 <amit.sinha2811@gmail.com>
2024-11-10 15:56:29 +0530

    Commit all files Nov 10 part 2

---
+++ November/ex_09112024_Literals/Task1_09112024.py
@@ -0,0 +1,7 @@
+# How to take the user input and print Quotient and Reminder
+num1= int(input("Enter the num1: "))
+num2= int(input("Enter the num2 :"))
+Quotient= num1 // num2
+Reminder= num1%num2
+print("The Quotient of two number is:", Quotient)
+print("The Reminder of two number", Reminder)
---
+++ November/ex_09112024_Literals/Task2_09112024.py
@@ -0,0 +1,13 @@
+# How to take the user input.
+# This Task 2 of Date 9/11/2024.
+num1= int(input("Enter the num1: "))
+num2= int(input("Enter the num2 :"))
+num3= int(input("Enter the num3 :"))
+sum= num1 + num2 + num3
+sub= num1- num2 -num3
+mul= num1*num2*num3
+div= num1/num2/num3
+print("The Sum of three number is:", sum)
+print("The difference of three number", sub)
+print("The Multiplication of three number is", mul)
+print("The division of three number is", div)
